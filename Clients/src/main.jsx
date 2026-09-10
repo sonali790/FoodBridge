87e783e31d185af4ebb0770d58c24968400968fa
Dipti <singh236dipti@gmail.com>
2026-09-10 21:49:10 +0530

    Import API_BASE_URL into main.jsx to restore full site rendering

--- Clients/src/main.jsx
+++ Clients/src/main.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
 import { ToastProvider } from './components/ToastContext.jsx'
+import { API_BASE_URL } from './config/api'
 
 axios.defaults.baseURL = API_BASE_URL
 
